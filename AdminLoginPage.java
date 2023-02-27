a6713a16e1f3460c2e0882a1cd6daf6fa5563053
Yogendra <yogendrasaini58558@gmail.com>
2023-02-27 22:23:51 +0530

    Testing IntelliJ

---
+++ AdminLoginPage.java
@@ -0,0 +1,7 @@
+import javax.swing.*;
+
+public class AdminLoginPage {
+    private JButton loginButton;
+    private JTextField usernameTextField;
+    private JPasswordField passwordPasswordField;
+}
