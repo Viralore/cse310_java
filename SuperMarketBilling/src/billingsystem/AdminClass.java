9e36d7bf6bb4354adab77105dfe744c122cb2e67
Yogendra <yogendrasaini58558@gmail.com>
2023-04-16 21:22:52 +0530

    Terminal Based

--- SuperMarketBilling/src/billingsystem/AdminClass.java
+++
@@ -1,8 +0,0 @@
-package billingsystem;
-
-public class AdminClass {
-    public String name;
-    public String email;
-    public String phone;
-    public String password;
-}
--- out/production/cse310_java/README.md
+++
@@ -1,4 +0,0 @@
-# cse310_java
-Project of Programming in Java
-Text editor In JAVA
-Contributers: Yogendra Abhishek Gopal
