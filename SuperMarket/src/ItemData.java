74e5e9aa20010be0ac7a1a927c76b0ab4611384c
Yogendra <yogendrasaini58558@gmail.com>
2023-04-16 21:43:03 +0530

    Terminal Based

--- SuperMarket/src/ItemData.java
+++ SuperMarket/src/ItemData.java
@@ -1,4 +1,9 @@
 public class ItemData
 {
-
+    String[][] list = new String[3][15];
+    public static void main(String[] args)
+    {
+        ItemData List_1 = new ItemData();
+        List_1.list[0][0] = "Hello";
+    }
 }
